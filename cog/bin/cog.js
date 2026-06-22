ac1b5ba014ebb47a3b4f407ded7280af6443d5e8
Alex Netkachov <alex.netkachov@gmail.com>
2026-06-22 03:20:09 +0100

    update

--- cog/bin/cog.js
+++ cog/bin/cog.js
@@ -1,11 +1,3 @@
 #!/usr/bin/env node
 
-import { runCli }
-  from '../dist/cli.js';
-
-await runCli(
-  process.argv.slice(2));
-
-process.exitCode = environment.exitCode;
-
-environment.logger.dispose();
+import '../dist/main.js';
